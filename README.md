2c66b94cf7d056449578356207bdeb89ecf15af6
esell <eujon@layered.com>
2015-07-20 15:11:46 -0600

    add ability to read in capture files

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 send all UDP traffic coming in on eth1, port 5060 to 10.10.10.10:5060:  
 
       udpreplay -i eth1 -f "udp and dst port 5060" -d "10.10.10.10:5060"
+	  udpreplay -r capture.pcap -f "udp and dst port 5060" -d "10.10.10.10:5060"
+
+
--- src/github.com/radiantiq/udpreplay/main.go
+++ src/github.com/radiantiq/udpreplay/main.go
@@ -11,13 +11,22 @@ import (
 )
 
 var iface = flag.String("i", "eth0", "Interface to get packets from")
+var fname = flag.String("r", "", "Filename to read from, overrides -i")
 var snaplen = flag.Int("s", 1600, "SnapLen for pcap packet capture")
 var filter = flag.String("f", "udp and dst port 5060", "filter for pcap")
 var dstHostPort = flag.String("d", "", "Destination host:port to replay traffic to")
 
 func main() {
 	flag.Parse()
-	handle, err := pcap.OpenLive(*iface, int32(*snaplen), true, pcap.BlockForever)
+	var handle *pcap.Handle
+	var err error
+	if *fname != "" {
+		log.Printf("Reading from pcap dump %q", *fname)
+		handle, err = pcap.OpenOffline(*fname)
+	} else {
+		log.Printf("Starting capture on interface %q", *iface)
+		handle, err = pcap.OpenLive(*iface, int32(*snaplen), true, pcap.BlockForever)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,7 +37,9 @@ func main() {
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		log.Println(packet.String())
+		if packet == nil {
+			return
+		}
 		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
 			udp, _ := udpLayer.(*layers.UDP)
 			fwdSIPPacket(udp.BaseLayer.Payload)
